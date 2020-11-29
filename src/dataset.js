92e555ba77ab2b4297e029062098f2bb049a7a24
tack-k <tyool9c@gmail.com>
2020-11-29 22:10:42 +0900

    データの追加

--- src/dataset.js
+++ src/dataset.js
@@ -1,18 +1,27 @@
 const dataset = {
-  wannaDo: {
-    content: "スキューバダイビング", url: "https://www.google.com/",
-    content: "スカイダイビング", url: "https://www.google.com/",
-    content: "世界一周旅行", url: "https://www.google.com/",
-    content: "逆立ち", url: "https://www.google.com/",
-    content: "南極旅行", url: "https://www.google.com/",
-    content: "フルマラソン", url: "https://www.google.com/",
-    content: "本の出版", url: "https://www.google.com/",
-    content: "気球に乗ってどこまでも", url: "https://www.google.com/",
-    content: "メジャリーグを見る", url: "https://www.google.com/",
-    content: "講演", url: "https://www.google.com/",
-    content: "ガッキーと友だちになる", url: "https://www.google.com/",
-    content: "「かぶと」のうなぎを食べる", url: "https://www.google.com/",
+  wannaDo: [
+    {content: "スキューバダイビング", url: "https://www.google.com/"},
+    {content: "スカイダイビング", url: "https://www.google.com/"},
+    {content: "世界一周旅行", url: "https://www.google.com/"},
+    {content: "逆立ち", url: "https://www.google.com/"},
+    {content: "南極旅行", url: "https://www.google.com/"},
+    {content: "フルマラソン", url: "https://www.google.com/"},
+    {content: "本の出版", url: "https://www.google.com/"},
+    {content: "気球に乗ってどこまでも", url: "https://www.google.com/"},
+    {content: "メジャリーグを見る", url: "https://www.google.com/"},
+    {content: "講演", url: "https://www.google.com/"},
+    {content: "ガッキーと友だちになる", url: "https://www.google.com/"},
+    {content: "「かぶと」のうなぎを食べる", url: "https://www.google.com/"},
+    {content: "ピラミッドに行く", url: "https://www.google.com/"},
+    {content: "トライアスロン", url: "https://www.google.com/"},
+    {content: "SATOブリアンに行く", url: "https://www.google.com/"},
+    {content: "オリジナルアプリをリリースする", url: "https://www.google.com/"},
+    {content: "哲学する", url: "https://www.google.com/"},
+    {content: "ポケモンを151匹集める", url: "https://www.google.com/"},
+    {content: "ベルセルクが完結するまで生きる", url: "https://www.google.com/"},
+    {content: "そらとぶ車に乗る", url: "https://www.google.com/"},
+  ]
   }
-}
+
 
 export default dataset
