8423fd193c3a2129d15f6a2c17a3410714dffb33
tack-k <tyool9c@gmail.com>
2020-11-29 15:42:10 +0900

    初期データセット

---
+++ src/dataset.js
@@ -0,0 +1,18 @@
+const dataset = {
+  wannaDo: {
+    content: "スキューバダイビング", url: "https://www.google.com/",
+    content: "スカイダイビング", url: "https://www.google.com/",
+    content: "世界一周旅行", url: "https://www.google.com/",
+    content: "逆立ち", url: "https://www.google.com/",
+    content: "南極旅行", url: "https://www.google.com/",
+    content: "フルマラソン", url: "https://www.google.com/",
+    content: "本の出版", url: "https://www.google.com/",
+    content: "気球に乗ってどこまでも", url: "https://www.google.com/",
+    content: "メジャリーグを見る", url: "https://www.google.com/",
+    content: "講演", url: "https://www.google.com/",
+    content: "ガッキーと友だちになる", url: "https://www.google.com/",
+    content: "「かぶと」のうなぎを食べる", url: "https://www.google.com/",
+  }
+}
+
+export default dataset
