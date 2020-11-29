9d598137df094f828cbe1b59250062e2169ff40e
tack-k <tyool9c@gmail.com>
2020-11-29 13:59:37 +0900

    material UI導入

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import './App.css';
 
-const App = () => {
+function App() {
   return (
     <div>
-      
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
