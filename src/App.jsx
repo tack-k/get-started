b9fa9066b0e3bb74f398770b2631a22d02a024e0
tack-k <tyool9c@gmail.com>
2020-11-29 22:11:27 +0900

    クリック時の表示切り替え

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Dataset from './dataset';
+import { WannaDo, ButtonSelect } from './components/index'
 
 const App = () => {
+
+  const [dataset, setDataset] = useState(Dataset);
+  const [content, setContent] = useState("やりたいことを見つける");
+  const [url, setUrl] = useState("");
+  const [like, setLike] = useState([])
+  const [dislike, setDisLike] = useState([])
+  const [likeDisabled, setLikeDisabled] = useState(true)
+  const [DisLikeDisabled, setDisLikeDisabled] = useState(true)
+  const [themeDisabled, setThemeDisabled] = useState(false)
+
+  
+  
+  
+  const changeContent = () => {
+    const wannaDoData = dataset.wannaDo;
+    const random = Math.floor(Math.random() * (wannaDoData.length));
+    const WannaDoContent = wannaDoData[random].content
+    setContent(WannaDoContent);
+    wannaDoData.splice(random, 1)
+    setDataset(dataset)
+    setLikeDisabled(false)
+    setDisLikeDisabled(false)
+    setThemeDisabled(true)
+
+  }
+
+  const setDisable = () => {
+    setLikeDisabled(true)
+    setDisLikeDisabled(true)
+    setThemeDisabled(false)
+  }
+
+  const addLike = () => {
+    const newLike = [...like, content]
+    setLike(newLike)
+    setDisable();
+  }
+
+  const addDislike = () => {
+    const newDislike = [...dislike, content]
+    setDisLike(newDislike)
+    setDisable();
+  }
+
+
+
+
   return (
     <div className="bg">
       <div className="inner">
         <div className="do">
-          <p>やってみたいこと</p>
-          <button>スタート</button>
+          <WannaDo content={content} url={"url"} />
+          <ButtonSelect onClick={changeContent} buttonName={"次のテーマ"} disabled={themeDisabled} />
         </div>
         <div className="interesting">
           <div className="like">
-            <button>気になる</button>
+
+            <ButtonSelect onClick={addLike} buttonName={"気になる"} disabled={likeDisabled} />
             <div className="like-field">
-              ・スキューバダイビング
-          </div>
+              {like.map((value, index) => {
+                return (
+                  <div className="like-list" key={index.toString()}>
+                    <li>{value}</li>
+                  </div>
+                )
+              })
+              }
+            </div>
           </div>
           <div className="dislike">
-            <button>絶対無理</button>
-            <div className="like-field">
-              ・バンジージャンプ
+            <ButtonSelect onClick={addDislike} buttonName={"絶対無理"} disabled={DisLikeDisabled} />
+            <div className="dislike-field">
+              {dislike.map((value, index) => {
+                return (
+                  <div className="like-list" key={index.toString()}>
+                    <li>{value}</li>
+                  </div>
+                )
+              })
+              }
           </div>
           </div>
         </div>
